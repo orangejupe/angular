a5ca100895574cbcef4d2627ef7fe815d9d3d2e6
Roman Spiridonov <conrspi@partner.waters.com>
2022-08-09 00:04:24 +0400

    valid initial

---
+++ src/my-d-app/src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+  id: number = 10;
+  status: string = 'offline';
+
+  getStatus() {
+    return this.status;
+  }
+}
