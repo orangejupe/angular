73f961db5a2fea30b686ac0ce8aa5688eae74fa1
Roman Spiridonov <conrspi@partner.waters.com>
2022-08-19 16:12:13 +0400

    fix compilator issues

--- src/movies/movies/src/app/app.component.ts
+++ src/movies/movies/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {* } from './navbar.component/navbar.component'
 
 @Component({
   selector: 'app-root',
--- src/movies/movies/src/app/app.module.ts
+++ src/movies/movies/src/app/app.module.ts
@@ -2,18 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavbarComponentComponent } from './navbar.component/navbar.component.component';
-import { MoviesComponentComponent } from './movies.component/movies.component.component';
-import { CreatemovieComponentComponent } from './createmovie.component/createmovie.component.component';
-import { MovieComponentComponent } from './Movies/movie.component/movie.component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponentComponent,
-    MoviesComponentComponent,
-    CreatemovieComponentComponent,
-    MovieComponentComponent
   ],
   imports: [
     BrowserModule
